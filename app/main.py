5e55b271f6d601acfb896e6b0f29a2281a89b0b5
Lorna <lorna1661maria@gmail.com>
2023-04-15 00:26:43 -0400

    adds docs fix

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ from mangum import Mangum
 from fastapi import FastAPI
 from app.api.api_v1.api import router as api_router
 
-app = FastAPI()
+# add openapi_prefix="/dev" to fix docs in api getway
+app = FastAPI(openapi_prefix="/dev")
 
 
 @app.get("/")
 async def root():
     """root endpoint"""
-    return {"mesage": "hello cats"}
+    return {"mesage": "Welcome to Cats API"}
+
 
 app.include_router(api_router, prefix="/api/v1")
 handler = Mangum(app)
