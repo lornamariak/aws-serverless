08242ef7bf2bc8a3d3e5817dfa23cd882e2ddd3e
Lorna <lorna1661maria@gmail.com>
2023-04-15 00:25:49 -0400

    updates with s3 write

--- app/api/api_v1/endpoints/get_cats.py
+++ app/api/api_v1/endpoints/get_cats.py
@@ -1,8 +1,12 @@
 """Module to get a list of cat breeds"""
 import requests
+import boto3
+import uuid
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
+s3 = boto3.client("s3")
+bucket_name = "catsapi"
 
 def get_breeds():
     """get all the breeds available"""
@@ -41,12 +45,20 @@ def list_breeds():
     else:
         raise HTTPException(status_code=500, detail="Failed to retrieve cat breeds")
 
-
 @router.get("/{breed_name}")
 def get_breed_details(breed_name: str):
     """Endpoint to retrieve details of a specific cat breed"""
     breed = get_cat_type(breed_name)
     if breed:
+        # Write the breed details to S3
+        object_id = str(uuid.uuid4())
+        csv_data = ",".join([str(val) for val in breed.values()])
+        s3.put_object(
+            Bucket=bucket_name,
+            Key=f"{object_id}.csv",
+            Body=csv_data.encode("utf-8")
+        )
         return breed
     else:
         raise HTTPException(status_code=404, detail="Breed not found")
+    
